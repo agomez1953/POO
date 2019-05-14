4285d4df38f31b0e7e0e47200817702c8bfd73a6
alumno <2000afgt@gmail.com>
2019-05-14 18:52:01 -0500

    añadimos banos y ambientes

--- Casa.py
+++ Casa.py
@@ -1,6 +1,9 @@
     #Para crear una clase utilizo la palabra reservada class
 
 class Casa:
+    banos=0
+    ambientes=0
+
     def __init__(self, direccion):
         self.direccion = direccion
     def __repr__(self):
--- Provador_casas.py
+++ Provador_casas.py
@@ -6,3 +6,10 @@ casa_vecino = Casa('calle 124 carrera 456')
 casa_Homero = Casa('Evergreen terrace 123, Springfield')
 
 print(f'mi casa es{mi_casa},'' la del vecino es',{casa_vecino},' y la de homero es',{casa_Homero})
+#se puede acceder de ambas maneras
+mi_casa.banos = 2
+mi_casa.ambientes =4
+setattr(casa_vecino,"banos",3)
+setattr(casa_vecino,"ambientes",5)
+print(f'Mi casa tiene {mi_casa.ambientes} ambientes')
+print(f'La del vecino tiene {casa_vecino.ambientes} ambientes')
