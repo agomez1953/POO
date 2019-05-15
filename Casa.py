a6275254baa830b31793f2f71f50874d1e2bd4f3
alumno <2000afgt@gmail.com>
2019-05-14 19:07:32 -0500

    añadimos funcion equals

--- Casa.py
+++ Casa.py
@@ -5,6 +5,10 @@ class Casa:
         self.banos = 0
         self.ambientes = 0
         self.direccion = direccion
+
     def __repr__(self):
         return f'Casa ubicada en{self.direccion}'
+
+    def __eq__(self, other):
+        return self.banos == other.banos and self.ambientes == other.ambientes
 
--- Provador_casas.py
+++ Provador_casas.py
@@ -5,15 +5,23 @@ mi_casa = Casa('calle 123 carrera 456')
 casa_vecino = Casa('calle 124 carrera 456')
 casa_Homero = Casa('Evergreen terrace 123, Springfield')
 
-print(f'mi casa es{mi_casa},'' la del vecino es',{casa_vecino},' y la de homero es',{casa_Homero})
+print(f'mi casa es{mi_casa}, la del vecino es,{casa_vecino}, y la de homero es,{casa_Homero}')
 #se puede acceder de ambas maneras
 mi_casa.banos = 2
 mi_casa.ambientes =4
 setattr(casa_vecino,"banos",3)
 setattr(casa_vecino,"ambientes",5)
+
 print(f'Mi casa tiene {mi_casa.ambientes} ambientes')
 print(f'La del vecino tiene {casa_vecino.ambientes} ambientes')
+
 atributos = casa_Homero.__dict__
+
 # este for es para imprimir los atributos
 for llave in atributos:
     print(f'atributo {llave} con el valor {atributos[llave]}')
+
+if mi_casa == casa_vecino:
+    print('mi casa y la del vecino son iguales')
+else:
+    print('mi casa y la del vecino son diferentes')
